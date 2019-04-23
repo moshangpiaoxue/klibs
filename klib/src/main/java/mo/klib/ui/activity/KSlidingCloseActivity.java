7093938a88a05e27eb525310b0b65233192d2dc7
moshangpiaoxue <419360214@qq.com>
2019-04-23 15:24:06 +0800

    侧滑删除优化

--- klib/src/main/java/mo/klib/ui/activity/KSlidingCloseActivity.java
+++ klib/src/main/java/mo/klib/ui/activity/KSlidingCloseActivity.java
@@ -2,7 +2,6 @@ package mo.klib.ui.activity;
 
 import android.os.Bundle;
 
-
 import cn.bingoogolapple.swipebacklayout.BGASwipeBackHelper;
 import mo.klib.R;
 
@@ -13,8 +12,8 @@ import mo.klib.R;
  */
 public class KSlidingCloseActivity extends BActivity implements BGASwipeBackHelper.Delegate {
     /**
-    * 帮助类
-    */
+     * 帮助类
+     */
     protected BGASwipeBackHelper mSwipeBackHelper;
 
     @Override
@@ -30,25 +29,27 @@ public class KSlidingCloseActivity extends BActivity implements BGASwipeBackHelp
      * 初始化滑动返回。在 super.onCreate(savedInstanceState) 之前调用该方法
      */
     private void initSwipeBackFinish() {
-        mSwipeBackHelper = new BGASwipeBackHelper(this, this);
+        if (isInitSwipe()) {
+            mSwipeBackHelper = new BGASwipeBackHelper(this, this);
 
-        // 「必须在 Application 的 onCreate 方法中执行 BGASwipeBackManager.getInstance().init(this) 来初始化滑动返回」
-        // 下面几项可以不配置，这里只是为了讲述接口用法。
+            // 「必须在 Application 的 onCreate 方法中执行 BGASwipeBackManager.getInstance().init(this) 来初始化滑动返回」
+            // 下面几项可以不配置，这里只是为了讲述接口用法。
 
-        // 设置滑动返回是否可用。默认值为 true
-        mSwipeBackHelper.setSwipeBackEnable(false);
-        // 设置是否仅仅跟踪左侧边缘的滑动返回。默认值为 true
-        mSwipeBackHelper.setIsOnlyTrackingLeftEdge(true);
-        // 设置是否是微信滑动返回样式。默认值为 true
-        mSwipeBackHelper.setIsWeChatStyle(true);
-        // 设置阴影资源 id。默认值为 R.drawable.bga_sbl_shadow
-        mSwipeBackHelper.setShadowResId(R.drawable.bga_sbl_shadow);
-        // 设置是否显示滑动返回的阴影效果。默认值为 true
-        mSwipeBackHelper.setIsNeedShowShadow(true);
-        // 设置阴影区域的透明度是否根据滑动的距离渐变。默认值为 true
-        mSwipeBackHelper.setIsShadowAlphaGradient(true);
-        // 设置触发释放后自动滑动返回的阈值，默认值为 0.3f
-        mSwipeBackHelper.setSwipeBackThreshold(0.3f);
+            // 设置滑动返回是否可用。默认值为 true
+            mSwipeBackHelper.setSwipeBackEnable(false);
+            // 设置是否仅仅跟踪左侧边缘的滑动返回。默认值为 true
+            mSwipeBackHelper.setIsOnlyTrackingLeftEdge(true);
+            // 设置是否是微信滑动返回样式。默认值为 true
+            mSwipeBackHelper.setIsWeChatStyle(true);
+            // 设置阴影资源 id。默认值为 R.drawable.bga_sbl_shadow
+            mSwipeBackHelper.setShadowResId(R.drawable.bga_sbl_shadow);
+            // 设置是否显示滑动返回的阴影效果。默认值为 true
+            mSwipeBackHelper.setIsNeedShowShadow(true);
+            // 设置阴影区域的透明度是否根据滑动的距离渐变。默认值为 true
+            mSwipeBackHelper.setIsShadowAlphaGradient(true);
+            // 设置触发释放后自动滑动返回的阈值，默认值为 0.3f
+            mSwipeBackHelper.setSwipeBackThreshold(0.3f);
+        }
     }
 
     /**
@@ -59,6 +60,10 @@ public class KSlidingCloseActivity extends BActivity implements BGASwipeBackHelp
     @Override
     public boolean isSupportSwipeBack() {
         return true;
+    }
+
+    public boolean isInitSwipe() {
+        return false;
     }
 
     /**
@@ -89,10 +94,16 @@ public class KSlidingCloseActivity extends BActivity implements BGASwipeBackHelp
 
     @Override
     public void onBackPressed() {
-        // 正在滑动返回的时候取消返回按钮事件
-        if (mSwipeBackHelper.isSliding()) {
-            return;
+        if (mSwipeBackHelper != null) {
+            // 正在滑动返回的时候取消返回按钮事件
+            if (mSwipeBackHelper.isSliding()) {
+                return;
+            }
+            mSwipeBackHelper.backward();
+        } else {
+            super.onBackPressed();
         }
-        mSwipeBackHelper.backward();
+
     }
+
 }
