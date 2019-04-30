80d526eb79cae9a2bf38be7cc9181228b03eb5e8
moshangpiaoxue <419360214@qq.com>
2019-04-30 13:28:22 +0800

    优化logUtil、处理9.0获取版本信息崩溃

--- klib/src/main/java/mo/klib/utils/logUtils/LogUtil.java
+++ klib/src/main/java/mo/klib/utils/logUtils/LogUtil.java
@@ -111,9 +111,9 @@ public class LogUtil {
         StringBuilder logInfoStringBuilder = new StringBuilder();
         logInfoStringBuilder.append("         \n");
         logInfoStringBuilder.append("手机型号：" + Build.BRAND).append(SEPARATOR);
-        logInfoStringBuilder.append("app 名称：" + AppInfoUtil.getAppInfo().getName()).append(SEPARATOR);
-        logInfoStringBuilder.append("版  本 码：" + AppInfoUtil.getAppInfo().getVersionCode()).append(SEPARATOR);
-        logInfoStringBuilder.append("版  本 号：" + AppInfoUtil.getAppInfo().getVersionName()).append(SEPARATOR);
+//        logInfoStringBuilder.append("app 名称：" + AppInfoUtil.getAppInfo().getName()).append(SEPARATOR);
+//        logInfoStringBuilder.append("版  本 码：" + AppInfoUtil.getAppInfo().getVersionCode()).append(SEPARATOR);
+//        logInfoStringBuilder.append("版  本 号：" + AppInfoUtil.getAppInfo().getVersionName()).append(SEPARATOR);
         logInfoStringBuilder.append("线  程 id：" + Thread.currentThread().getId()).append(SEPARATOR);
         logInfoStringBuilder.append("线  程 名：" + Thread.currentThread().getName()).append(SEPARATOR);
         logInfoStringBuilder.append("文  件 名：" + stackTraceElement.getFileName()).append(SEPARATOR);
