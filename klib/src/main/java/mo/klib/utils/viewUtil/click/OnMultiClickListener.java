cfd21f366ae508c0aafb120e6b348d5759a4988c
moshangpiaoxue <419360214@qq.com>
2019-05-09 13:43:57 +0800

    优化多次点击监听

--- klib/src/main/java/mo/klib/utils/viewUtil/click/OnMultiClickListener.java
+++ klib/src/main/java/mo/klib/utils/viewUtil/click/OnMultiClickListener.java
@@ -5,21 +5,26 @@ import android.view.View;
 /**
  * @ author：mo
  * @ data：2018/12/28
- * @ 功能：
+ * @ 功能：快速点击监听（在规定时间内不能重复点击）
  */
 public abstract class OnMultiClickListener implements View.OnClickListener {
-    // 两次点击按钮之间的点击间隔不能少于1000毫秒
-    private static final int MIN_CLICK_DELAY_TIME = 1000;
-    private static long lastClickTime;
+    /**
+     * 两次点击按钮之间的点击间隔不能少于1000毫秒
+     */
+    private static long MIN_CLICK_DELAY_TIME = 1000;
+
+    public OnMultiClickListener() {
+    }
+
+    public OnMultiClickListener(long millis) {
+        MIN_CLICK_DELAY_TIME = millis;
+    }
 
     public abstract void onMultiClick(View v);
 
     @Override
     public void onClick(View v) {
-        long curClickTime = System.currentTimeMillis();
-        if ((curClickTime - lastClickTime) >= MIN_CLICK_DELAY_TIME) {
-            // 超过点击间隔后再将lastClickTime重置为当前点击时间
-            lastClickTime = curClickTime;
+        if (ClickUtil.isFastClick(MIN_CLICK_DELAY_TIME)) {
             onMultiClick(v);
         }
     }
